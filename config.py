240771d2ca49e8b44b4e5ad5f1e2358115b1be7c
SkOfficial01 <123543112+SkOfficial01@users.noreply.github.com>
2024-08-01 01:10:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ API_ID = "13279715"
 # -------------------------------------------------------------
 API_HASH = "56e198053932ecf216af72a2ddffcd78"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = getenv("OWNER_ID", "6407024039")
-SUPPORT_GRP = "officialbigdaddy07"
-UPDATE_CHNL = "officialbigdaddy07"
+BOT_TOKEN = getenv("BOT_TOKEN", "6902219084:AAGxU8rkD9iTr6LKl0bpIPCwJwhnt8CYRPo")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Mrdaxx123:Mrdaxx123@cluster0.q1da65h.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = getenv("OWNER_ID", "6971100005")
+SUPPORT_GRP = "https://t.me/+XwP5abJX8zJiODA1"
+UPDATE_CHNL = "https://t.me/+XwP5abJX8zJiODA1"
 OWNER_USERNAME = "ll_SANKI_XD"
