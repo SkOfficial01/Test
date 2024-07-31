07fb733f517588ce12f744ef9316dfafd6c39b3e
SkOfficial01 <123543112+SkOfficial01@users.noreply.github.com>
2024-08-01 01:11:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "13279715"
+API_ID = "22269028"
 # -------------------------------------------------------------
-API_HASH = "56e198053932ecf216af72a2ddffcd78"
+API_HASH = "2f3b05df7dd72f2769445245f84b3bab"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "6902219084:AAGxU8rkD9iTr6LKl0bpIPCwJwhnt8CYRPo")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Mrdaxx123:Mrdaxx123@cluster0.q1da65h.mongodb.net/?retryWrites=true&w=majority")
